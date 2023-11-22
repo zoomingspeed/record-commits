1177443a63e6cc6c6177dd5d4e25809b2244b0f1
zoomingspeed <134181140+zoomingspeed@users.noreply.github.com>
2023-11-22 06:05:11 +0000

    make final changes to the commiter before trying to get world record

--- commiter.py
+++ commiter.py
@@ -3,7 +3,10 @@ import os
 
 commit_amount = int(input("Enter how many commits you want infinitely made: "))
 
-while True:
-    for i in range(commit_amount):
-        os.system(f'git commit --allow-empty -m "Commit {i} of {commit_amount}"')
-    os.system("git push")
+if commit_amount > 50000:
+    print("No! That takes too long to do. Maximum commit amount is fifty thousand, rerun this file and try again.")
+else:
+    while True:
+        for i in range(commit_amount):
+            os.system(f'git commit --allow-empty -m "Commit {i} of {commit_amount}"')
+        os.system("git push")
